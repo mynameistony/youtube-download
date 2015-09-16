442ec9c7e13e31f3e11beb32ff07af982fecb868
Tony (Broken Keys) <tonyrogers10195@gmail.com>
2015-09-16 11:12:07 -0700

    Fucking a

--- clearData.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-rm logs/*
-#rm downloads/*
--- extract-tags.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-artist=$(echo $@ | grep "^[[:print:]]* - " -o | sed s/" - $"/""/g)
-track=$(echo $@ | grep " - [[:print:]]*\.mp3" -o | sed -e s/"^ - "/""/g -e s/"-[-A-Za-z0-9_]*\.mp3$"/""/g)
-
-id3v2 -a "$artist" "$@"
-id3v2 -t "$track" "$@"
-id3v2 -R "$@"
